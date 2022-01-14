871f13f761dbe48f87c22550b689041a8b30d225
newvijay <newvijay.babu@gmail.com>
2022-01-14 15:37:15 -0600

    added host

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'z+ksf@)0d^qojbh4rnp4b1to$hq&*tt(3bs$gf(3i267g$k9ln'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','newtrynow.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','newhair.herokuapp.com']
 
 # Application definition
 
